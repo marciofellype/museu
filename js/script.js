a1b1078cfb1da182aeba4470102b128ce0811b09
marciofellype <37717831+marciofellype@users.noreply.github.com>
2022-11-09 21:19:15 -0300

    jy

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+window.sr = ScrollReveal({ reset:true });
+
+
+sr.reveal('.container', {
+    rotate: { x:0, y:80, z:0 },
+    duration: 1500
+})
+
+sr.reveal('.title', {
+    rotate: { x:0, y:80, z:0 },
+    duration: 1500
+})
